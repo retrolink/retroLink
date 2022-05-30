ca50d61447becedaaba49cb75f5a94d648413757
enricodias <enricodias@gmail.com>
2022-05-30 18:41:30 -0300

    Migrate chrome addon to manifest v3

--- addons/chrome/background.js
+++ addons/chrome/background.js
@@ -1,8 +1,6 @@
-chrome.browserAction.onClicked.addListener(
-	function (tab) {
-		chrome.tabs.create({
-			'url': 'https://retrolink.com.br/api/?addon=chrome&url=' + tab.url,
-			'selected': true
-		});
-	}
-);
+chrome.action.onClicked.addListener((tab => {
+	chrome.tabs.create({
+		'url': 'https://retrolink.com.br/api/?addon=chrome&url=' + tab.url,
+		'selected': true
+	});
+}));
